01332168ae19dae6ea3f8101a7fed9ebd8e667d6
Pedro De Oliveira <pedro.deoliveira@gaviglio.com>
2023-10-24 15:14:08 -0300

    ajustes para castellano

--- Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace Web.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Recuérdame")]
             public bool RememberMe { get; set; }
         }
 
