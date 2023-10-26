b6a2faefce04aa84d7a001453e7219575d87b040
Pedro De Oliveira <pedro.deoliveira@gaviglio.com>
2023-10-26 17:14:43 -0300

    add importación lista por excel

--- Web/Controllers/ListaPrecioController.cs
+++ Web/Controllers/ListaPrecioController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using EFCore.BulkExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Web.Models;
 using Web.Repos;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Web.Controllers
 {
@@ -172,5 +173,94 @@ namespace Web.Controllers
         {
           return (_context.ListaPrecios?.Any(e => e.Id == id)).GetValueOrDefault();
         }
+
+        public IActionResult ImportarListaPrecio()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("MostrarDatos")]
+        public IActionResult MostrarDatos([FromForm] IFormFile ArchivoExcel)
+        {
+            Stream stream = ArchivoExcel.OpenReadStream();
+
+            IWorkbook MiExcel = null;
+
+            if (Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
+            {
+                MiExcel = new XSSFWorkbook(stream);
+            }
+            else
+            {
+                MiExcel = new HSSFWorkbook(stream);
+            }
+
+            ISheet HojaExcel = MiExcel.GetSheetAt(0);
+
+            int cantidadFilas = HojaExcel.LastRowNum;
+
+            List<ListaPrecio> lista = new List<ListaPrecio>();
+
+            for (int i = 1; i <= cantidadFilas; i++)
+            {
+
+                IRow fila = HojaExcel.GetRow(i);
+
+                lista.Add(new ListaPrecio
+                {
+                    Descripcion = fila.GetCell(0).ToString(),
+                    FechaHasta = DateTime.Parse(fila.GetCell(1).ToString()),
+                    CondicionPagoRefId = Int16.Parse(fila.GetCell(2).ToString()),
+                    Precio = Decimal.Parse(fila.GetCell(3).ToString()),
+                    FechaRegistro = DateTime.Now
+
+                });
+            }
+
+            return StatusCode(StatusCodes.Status200OK, lista);
+        }
+
+        [HttpPost, ActionName("EnviarDatos")]
+        public IActionResult EnviarDatos([FromForm] IFormFile ArchivoExcel)
+        {
+            Stream stream = ArchivoExcel.OpenReadStream();
+
+            IWorkbook MiExcel = null;
+
+            if (Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
+            {
+                MiExcel = new XSSFWorkbook(stream);
+            }
+            else
+            {
+                MiExcel = new HSSFWorkbook(stream);
+            }
+
+            ISheet HojaExcel = MiExcel.GetSheetAt(0);
+
+            int cantidadFilas = HojaExcel.LastRowNum;
+            List<ListaPrecio> lista = new List<ListaPrecio>();
+
+            for (int i = 1; i <= cantidadFilas; i++)
+            {
+
+                IRow fila = HojaExcel.GetRow(i);
+
+                lista.Add(new ListaPrecio
+                {
+                    Descripcion = fila.GetCell(0).ToString(),
+                    FechaHasta = DateTime.Parse(fila.GetCell(1).ToString()),
+                    CondicionPagoRefId = Int16.Parse(fila.GetCell(2).ToString()),
+                    Precio = Decimal.Parse(fila.GetCell(3).ToString()),
+                    FechaRegistro = DateTime.Now
+
+                });
+            }
+
+            _context.BulkInsert(lista);
+
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok" });
+        }
+
     }
 }
